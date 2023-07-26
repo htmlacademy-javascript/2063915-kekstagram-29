8c2748ab6f0f84cd9c17aa2f98ec48acc2310295
Anna <ann_dmitirevna97@mail.ru>
2023-07-26 18:03:18 +0300

    Надо подкачаться

--- js/util.js
+++ js/util.js
@@ -7,8 +7,27 @@ const getRandomInteger = (a, b) => {
 
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-const isEscapeKey = (evt) => evt.key === 'Escape';
+const isEscapeKey = (evt) => evt.key === 'Escape'
 
 const isEnterKey = (evt) => evt.key === 'Enter';
 
-export {getRandomArrayElement, getRandomInteger, isEscapeKey, isEnterKey};
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+
+export {getRandomArrayElement, getRandomInteger, isEscapeKey, isEnterKey, shuffleArray, debounce};
