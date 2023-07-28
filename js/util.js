e7f1e758f0c48fb28497bf6f4613171ddddabb6a
Anna <ann_dmitirevna97@mail.ru>
2023-07-28 17:19:57 +0300

    Павки 5

--- js/util.js
+++ js/util.js
@@ -1,16 +1,3 @@
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-const isEscapeKey = (evt) => evt.key === 'Escape';
-
-const isEnterKey = (evt) => evt.key === 'Enter';
-
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -29,5 +16,4 @@ function debounce (callback, timeoutDelay = 500) {
   };
 }
 
-
-export {getRandomArrayElement, getRandomInteger, isEscapeKey, isEnterKey, shuffleArray, debounce};
+export {shuffleArray, debounce};
